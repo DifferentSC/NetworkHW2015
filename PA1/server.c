db122be87f20bf113ffdcacf922d1a9328cc9dbd
Gyewon Lee <strayyyyyy@gmail.com>
2015-09-22 15:26:42 +0900

    Implement request and ACK receive handling

--- PA1/server.c
+++ PA1/server.c
@@ -26,8 +26,34 @@ char* server_packet_to_bytes(struct server_packet packet) {
     return return_byte;
 }
 
+void delete_and_shift(struct server_packet *window, int *window_count) {
+    int i;
+    for(i = 1; i < *window_count; i ++) {
+        window[i-1] = window[i];
+    }
+    *window_count--;
+}
+
+int read_file(FILE *fp, struct server_packet *packet, short *sn) {
+    int i;
+    
+    packet->sn = *sn;
+    *sn++;
+    for (i = 0; i < BUFFER_SIZE - 2; i ++) {
+        char temp;
+        if ((temp = fgetc(fp)) == EOF) {
+            return 1;
+        } else {
+            packet->file_data[i] = temp;
+        }
+    }
+    return 0;
+}
+
 int main (int argc, char **argv) {
    
+    char *filename[3] = {"./data/TransferMe10.mp4", "./data/TransferMe20.mp4", "./data/TransferMe30.mp4"};
+
 	// Check arguments
     if (argc != 3) {
         printf("Usage: %s <port> <window_size>\n", argv[0]);
@@ -39,7 +65,7 @@ int main (int argc, char **argv) {
     int socket_fd;
     // Opening socket
     if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        printf("Opening socket failed!");
+        printf("Opening socket failed!\n");
         exit(1);
     }
 
@@ -52,18 +78,22 @@ int main (int argc, char **argv) {
     server_addr.sin_port = htons(port);
     // Binding
     if (bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        printf("Binding failed! bind() returned");
+        printf("Binding failed! bind() returned\n");
         exit(1);
     }
 
     if (listen(socket_fd, 1) < 0) {
-        printf("Listening to socket failed!");
+        printf("Listening to socket failed!\n");
         exit(1);
     }
 
     printf("Start waiting...");
 
     struct server_packet *window = (struct server_packet *)malloc(window_size * sizeof(struct server_packet));
+    int window_count;
+    FILE *fp = NULL;
+    int is_read_finished = 1;
+    short sn;
 
     while(1) {
         // accept client fd
@@ -72,21 +102,58 @@ int main (int argc, char **argv) {
         int client_fd = accept(socket_fd, (struct sockaddr *)&client_addr, &len);
         if (client_fd < 0) {
             printf("Accepting client request failed!\n");
-            continue;
+            exit(1);
         }
         char client_message[CLIENT_MESSAGE_SIZE];
         int message_size = read(client_fd, client_message, CLIENT_MESSAGE_SIZE);
         if (message_size != CLIENT_MESSAGE_SIZE) {
-            printf("Invalid client message size!");
-            continue;
+            printf("Invalid client message size!\n");
+            exit(1);
         }
         if (client_message[0] == 'A') {
+            short client_sn = client_message[1] * 256 + client_message[2];
             // ACK
+            if (window_count == 0) {
+                printf("Received ACK without sending message!\n");
+                exit(1);
+            }
+            if (fp == NULL) {
+                printf("Received ACK before fp is set!");
+                exit(1);
+            }
+            if (window[0].sn != client_sn) {
+                printf("ACK or message is missing!\n");
+                exit(1);
+            }
+            delete_and_shift(window, &window_count);
+            if (!is_read_finished) {
+                while(window_count < window_size && !is_read_finished) {
+                    is_read_finished = read_file(fp, &window[window_count], &sn);
+                    window_count++;
+                }
+            }
         } else if (client_message[0] == 'R') {
             // Request
+            int file_choice = client_message[1];
+            if (file_choice < 0 || file_choice > 2) {
+                printf("Invalid file number!\n");
+                exit(1);
+            }
+            if (!is_read_finished) {
+                printf("Get another request before the previous request is finished!\n");
+                exit(1);
+            }
+            fp = fopen(filename[file_choice], "r");
+            is_read_finished = 0;
+            window_count = 0;
+            sn = 0;
+            while(window_count < window_size && !is_read_finished) {
+                is_read_finished = read_file(fp, &window[window_count], &sn);
+                window_count++;
+            }
         } else {
-            printf("Invalid client request!");
-            continue;
+            printf("Invalid client request!\n");
+            exit(1);
         }
     }
 
